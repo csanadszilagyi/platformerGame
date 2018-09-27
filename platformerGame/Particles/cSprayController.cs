cc4e5afecb052636df318f231210d89ba3e93d7e
jourdonnais <szcsani@gmail.com>
2018-09-27 08:57:02 +0200

    very cool fuel effect

--- platformerGame/Particles/cSprayController.cs
+++ platformerGame/Particles/cSprayController.cs
@@ -18,7 +18,7 @@ namespace platformerGame.Particles
 
         public cSprayController(cParticleManager manager, uint max_particles = 300) : base(manager, max_particles)
         {
-            this.renderStates.Texture = manager.ExplosionTexture; // smoke
+            this.renderStates.Texture = manager.SmokeTexture; // smoke
         }
 
         private void empower(Particle particle)
@@ -26,20 +26,21 @@ namespace platformerGame.Particles
             particle.Pos = particle.StartPos;
             particle.LastPos = particle.Pos;
             particle.ViewPos = particle.Pos;
-            particle.MaxSpeed = cAppMath.GetRandomNumber(40, 100); //200,400 | 700, 900 | (400, 600); //3, 8//Math->GetRandomNumber(510, 800); // 2000.0f
+            particle.MaxSpeed = cAppMath.GetRandomNumber(30, 50); //200,400 | 700, 900 | (400, 600); //3, 8//Math->GetRandomNumber(510, 800); // 2000.0f
 
             //------------------------------------------------------------------------------------------------
             float angle = (float)cAppMath.DegressToRadian(cAppMath.GetRandomNumber(0, 360));//sDivisions * m_Angles;
+            particle.Rotation = angle;
 
-            Vector2f dirUnit = cAppMath.GetRandomUnitVec();
-                //cAppMath.GetRandomVecBySpread(new Vector2f(0.5f, -1.0f), cAppMath.DegressToRadian(3));
+            // Vector2f dirUnit = cAppMath.GetRandomUnitVec();
+            Vector2f dirUnit = cAppMath.GetRandomVecBySpread(new Vector2f(0.0f, -1.0f), cAppMath.DegressToRadian(6));
 
             particle.Vel = dirUnit * particle.MaxSpeed;
             //------------------------------------------------------------------------------------------------
             //------------------------------------------------------------------------------------------------
 
             //particle.m_Vel = sf::Vector2f(Math->GetRandomClamped() * particle.m_MaxSpeed, Math->GetRandomClamped() *particle.m_MaxSpeed);
-            particle.SlowDown = 0.98f; //0.92f;
+            particle.SlowDown = 1.0f; //0.92f;
                                        //particle.m_SlowDown = (float)Math->GetRandomDoubleInRange(0.55, 0.7); //0.6f;
                                        //phs->AddForce( sf::Vector2f(Math->GetRandomClamped() * phs->MaxSpeed, Math->GetRandomClamped() * phs->MaxSpeed) );
 
@@ -49,11 +50,12 @@ namespace platformerGame.Particles
 
             particle.Dims = new Vector2f(uSize.X * particle.Scale, uSize.Y * particle.Scale);
 
-            particle.ScaleSpeed = -cAppMath.GetRandomNumber(15, 40);
+            particle.ScaleSpeed = (float)cAppMath.GetRandomNumber(15, 25) / 100.0f;
+
             particle.Color = Utils.GetRandomColor(250, 250, 1, 160, 0, 0); // GetRandomRedColor();
             particle.Opacity = 255.0f;
             particle.Life = 15.0f;
-            particle.Fade = cAppRandom.GetRandomNumber(100, 400);
+            particle.Fade = cAppRandom.GetRandomNumber(10, 40);
 
             particle.Intersects = false;
         }
@@ -71,13 +73,12 @@ namespace platformerGame.Particles
             Particle particle = emission.Particle;
             particle.StartPos = emission.StartPosition;
             this.empower(particle);
-            
         }
 
         public void Emit(EmissionInfo emission)
         {
-            minScale = 0.5;
-            maxScale = 0.9;
+            minScale = 0.1;
+            maxScale = 0.35;
             
             loopAddition(emission, 40);
         }
@@ -90,21 +91,24 @@ namespace platformerGame.Particles
             {
                 Particle p = pool.get(i);
 
-                p.Vel.Y += (Constants.GRAVITY*30 * (step_time * step_time));
+                // p.Vel.Y += (Constants.GRAVITY*30 * (step_time * step_time));
 
                 p.Vel.X *= p.SlowDown;
                 p.Vel.Y *= p.SlowDown;
 
                 cAppMath.Vec2Truncate(ref p.Vel, p.MaxSpeed);
 
-                world.collideParticleSAT(p, step_time);
+                //world.collideParticleSAT(p, step_time);
 
                 //p.Heading = cAppMath.Vec2NormalizeReturn(p.Vel);
+
+                Vector2u uSize = this.renderStates.Texture.Size;
+                p.Scale -= p.ScaleSpeed * step_time;
+                p.Dims = new Vector2f(uSize.X * p.Scale, uSize.Y * p.Scale);
+
                 p.LastPos = p.Pos;
                 p.Pos.X += p.Vel.X * step_time;
                 p.Pos.Y += p.Vel.Y * step_time;
-
-
 
                 if (!cAppMath.Vec2IsZero(p.Vel))
                 {
@@ -112,13 +116,14 @@ namespace platformerGame.Particles
                     // p.Rotation = (float)cAppMath.GetAngleOfVector(p.Heading);
                 }
 
-                //world.collideParticleRayTrace(p, step_time, false, false);             
+                world.collideParticleRayTrace(p, step_time, true, false);             
 
                 p.Opacity -= p.Fade * step_time;
 
-                if (p.Opacity <= 0.0f)
+                if (p.Opacity <= 0.0f || p.Scale <= 0.0f)
                 {
                     p.Opacity = 0.0f;
+                    p.Scale = 0.0f;
                     p.Life -= 1.0f;
 
                     if (p.Life <= 0.0f)
@@ -147,30 +152,42 @@ namespace platformerGame.Particles
             float tSizeX = uSize.X;
             float tSizeY = uSize.Y;
 
-            Vector2f newDims = new Vector2f();
+            Vector2f halfDims = new Vector2f();
 
             for (int i = 0; i < pool.CountActive; ++i)
             {
                 Particle p = pool.get(i);
 
-                newDims.X = p.Dims.X / division;
-                newDims.Y = p.Dims.Y / division;
+                halfDims.X = p.Dims.X / division;
+                halfDims.Y = p.Dims.Y / division;
 
                 p.ViewPos = cAppMath.Interpolate(p.LastPos, p.Pos, alpha);
 
                 uint vertexIndex = (uint)i * multiplier;
 
-                Vector2f v0Pos = new Vector2f(p.ViewPos.X - newDims.X,
-                                              p.ViewPos.Y - newDims.Y);
+                Vector2f v0Pos = new Vector2f(p.ViewPos.X - halfDims.X,
+                                              p.ViewPos.Y - halfDims.Y);
 
-                Vector2f v1Pos = new Vector2f(p.ViewPos.X + newDims.X,
-                                              p.ViewPos.Y - newDims.Y);
+                Vector2f v1Pos = new Vector2f(p.ViewPos.X + halfDims.X,
+                                              p.ViewPos.Y - halfDims.Y);
 
-                Vector2f v2Pos = new Vector2f(p.ViewPos.X + newDims.X,
-                                              p.ViewPos.Y + newDims.Y);
+                Vector2f v2Pos = new Vector2f(p.ViewPos.X + halfDims.X,
+                                              p.ViewPos.Y + halfDims.Y);
 
-                Vector2f v3Pos = new Vector2f(p.ViewPos.X - newDims.X,
-                                              p.ViewPos.Y + newDims.Y);
+                Vector2f v3Pos = new Vector2f(p.ViewPos.X - halfDims.X,
+                                              p.ViewPos.Y + halfDims.Y);
+
+
+                // if want rotation included:
+                Transform rot = Transform.Identity;
+                rot.Rotate((float)cAppMath.RadianToDegress(p.Rotation), p.ViewPos);
+               
+                v0Pos = rot.TransformPoint(v0Pos);
+                v1Pos = rot.TransformPoint(v1Pos);
+                v2Pos = rot.TransformPoint(v2Pos);
+                v3Pos = rot.TransformPoint(v3Pos);
+
+                // END of rotation code
 
                 // Top-left
                 Vertex v0 = new Vertex(
@@ -209,6 +226,8 @@ namespace platformerGame.Particles
 
         public override void Render(RenderTarget destination, float alpha)
         {
+            if (this.pool.CountActive > 0)
+                DrawingBase.DrawTextureSimple(destination, this.pool.get(0).StartPos, this.renderStates.Texture, new IntRect(), Color.Yellow, BlendMode.Alpha);
             destination.Draw(vertices, renderStates);
         }
     }
